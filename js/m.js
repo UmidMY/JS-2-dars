56fcc271f1978c864602941337c5a956c5ff0762
Umid Muyassarov <168184714+UmidMY@users.noreply.github.com>
2024-05-06 17:34:32 +0500

    JS 2-dars
    Zeromax IT-Center

---
+++ js/m.js
@@ -0,0 +1,38 @@
+/*console.log("Men Faqat konsol oynasida ko'rinaman!");*/
+
+/*alert("Saytimizga Xush kelibsiz!");*/
+
+/*document.write(4014+12526-2525+25235);*/
+
+/*let x =5;
+
+let z =7;
+
+x =10;*/
+/*
+let salom ="Assalom aleykum";
+*/
+
+/*
+let x =10,z = 7, y = 25
+console.log (z+y+x);*/
+
+/*let soz = "JavaScript";
+
+let son = 5;*/
+
+/*
+document.write(son);*/
+
+let x =1;
+/*x +=5;*/
+/*x -=2;*/
+/*x *=5;*/
+/*x /=2;*/
+/*let qoldiq = x**3;*/
+/*++x;*/
+/*x++;*/
+/*x--;*/
+/*--x;*/
+
+document.write(x)
